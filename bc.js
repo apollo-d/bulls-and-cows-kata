52f2237bbcea0a198c807719bf3a77272f15393b
paul.diehl <paul.diehl@availcarsharing.com>
2021-04-07 15:47:04 -1000

    add solution

--- bc.js
+++ bc.js
@@ -4,5 +4,72 @@ const { readData,
     generateRandomDigit } = require('./helpers')
 
 exports.guess = async (guess) => {
-
+    if (!guess.match(matchFourNonRepeatingNumber)) throw new Error()
+    let { matchResults = [], secret } = await readData()
+    if (!secret)
+        secret = await _generateAndStoreSecret()
+    const matchResult = _getBullsAndCows(guess, secret)
+    const newEntry = {
+        attempt: matchResults.length + 1,
+        guess,
+        matchResult
+    }
+    if (newEntry.matchResult === '4B0C') {
+        await writeData('')
+    } else {
+        await writeData({matchResults: [...matchResults, newEntry], secret})
+    }
+    console.log([...matchResults, newEntry])
+    return [...matchResults, newEntry]
 }
+
+const _getBullsAndCows = (guess, secret) => {
+    let bulls = 0, cows = 0;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (i == j && guess[i] === secret[j]) {
+                bulls++
+            } else if (guess[i] === secret[j]) {
+                cows++
+            }
+        }
+    }
+    return `${bulls}B${cows}C`
+}
+
+// RECURSIVE APPROACH
+const _generateAndStoreSecret = async () => {
+    let secret = ''
+    for(let i = 0; i < 4; i++) {
+        secret += _generateSecret(secret)
+    }
+    await writeData({ secret })
+    return secret
+}
+
+const _generateSecret = (secret ) => {
+    const _secret = generateRandomDigit()
+    if (secret.indexOf(_secret) !== -1) {
+        return _generateSecret(secret)
+    }
+    return _secret
+}
+
+
+
+
+// PICK FROM LIST APPROACH - but THIS IMPLEMENTATION IS WRONG!
+// const _generateAndStoreSecret = async () => {
+//     let secret = ''
+//     let numbers = [...Array(10).keys()];
+//     for(let i = 0; i < 4; i++) {
+//         let randomNumber = numbers[Math.floor(Math.random() * numbers.length)]
+//         const index = numbers.indexOf(randomNumber);
+//         if (index > -1) {
+//             numbers.splice(index, 1);
+//         }
+//         secret += randomNumber.toString()
+//     }
+//     await writeData({ secret })
+//     return secret
+// }
--- bc.test.js
+++ bc.test.js
@@ -2,28 +2,52 @@ const { guess } = require('./bc')
 const { readData, writeData, matchFourNonRepeatingNumber } = require('./helpers')
 
 //ACTIVITY 1
-xtest('should throw error for invalid guess', async () => {
-
+test('should throw error for invalid guess', async () => {
+    await expect(guess('123')).rejects.toThrow(Error)
+    await expect(guess('1122')).rejects.toThrow(Error)
+    await expect(guess('!123')).rejects.toThrow(Error)
+    await expect(guess('A123')).rejects.toThrow(Error)
+    await expect(guess('01234')).rejects.toThrow(Error)
 })
 
 //ACTIVITY 2
-xtest('should generate secret string and store value', async () => {
-
+test('should generate secret string and store value', async () => {
+    await guess('1234')
+    const { secret } = await readData()
+    expect(secret).toBeDefined()
+    expect(typeof secret).toBe('string')
 });
 
-xtest('should generate secret string to meet criterion', async () => {
-
+test('should generate secret string to meet criterion', async () => {
+    await guess('1234')
+    const { secret } = await readData()
+    expect(secret).toMatch(matchFourNonRepeatingNumber)
 })
 
-xtest('should only generate secret string when none exists', async () => {
-
+test('should only generate secret string when none exists', async () => {
+    await writeData({ secret: '4321'})
+    await guess('1234')
+    const { secret } = await readData()
+    expect(secret).toBe('4321')
 })
 
 //ACTIVITY 3
-xtest('should store and return list of match results for incorrect guess', async () => {
-
+test('should store and return list of match results for incorrect guess', async () => {
+    await writeData({ secret: '4321'})
+    const _guess = '1234'
+    const returnedResults = await guess(_guess)
+    const { matchResults } = await readData();
+    const expectedResults = [{ attempt: 1, guess: '1234', matchResult: '0B4C' }];
+    expect(returnedResults).toMatchObject(expectedResults)
+    expect(matchResults).toMatchObject(expectedResults)
 })
 
-xtest('should return list of match results for correct guess and clear store', async () => {
-
+test('should return list of match results for correct guess and clear store', async () => {
+    await writeData({ secret: '4321'})
+    const _guess = '4321'
+    const returnedResults = await guess(_guess)
+    const { matchResults } = await readData();
+    const expectedResults = [{ attempt: 1, guess: '4321', matchResult: '4B0C' }];
+    expect(returnedResults).toMatchObject(expectedResults)
+    expect(matchResults).toBeUndefined()
 })
